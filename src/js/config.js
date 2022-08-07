22434a2d90e67847e636375f72d82bdcac180d2f
Moonreaper205 <moonreaper@gmail.com>
2022-08-07 14:04:16 -0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '650e8c2a-8e5e-461e-a395-1cb078cddfaf';
+export const MODAL_CLOSE_SEC = 2.5;
